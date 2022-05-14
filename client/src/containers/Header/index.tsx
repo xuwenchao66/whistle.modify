4dfe7a78d5ce642b2f0ea66daf5ba9e7f37f95c7
xuwenchao <842390367@qq.com>
2022-05-14 15:58:12 +0800

    feat: 🎸 page header extra

--- client/src/containers/Header/index.tsx
+++ client/src/containers/Header/index.tsx
@@ -4,24 +4,21 @@ import { PlusOutlined, FullscreenOutlined } from '@ant-design/icons';
 import { inIframe } from '@/utils';
 
 const Header: FC = memo(() => {
-  return (
-    <PageHeader
-      title={'whistle.modify'}
-      extra={[
-        <Button type="primary" icon={<PlusOutlined />}>
-          Create
-        </Button>,
-        inIframe() ? (
-          <Tooltip title="Open in a separate tab" placement="bottomLeft">
-            <Button
-              icon={<FullscreenOutlined />}
-              onClick={() => window.open(window.location.href)}
-            />
-          </Tooltip>
-        ) : null,
-      ]}
-    />
-  );
+  const extra = [
+    <Button key="create" type="primary" icon={<PlusOutlined />}>
+      Create
+    </Button>,
+    inIframe() ? (
+      <Tooltip key="open" title="Open in a separate tab" placement="bottomLeft">
+        <Button
+          icon={<FullscreenOutlined />}
+          onClick={() => window.open(window.location.href)}
+        />
+      </Tooltip>
+    ) : null,
+  ];
+
+  return <PageHeader title={'whistle.modify'} extra={extra} />;
 });
 
 export default Header;
