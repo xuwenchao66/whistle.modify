8d3e05d6d1cd57b90781ff79cc2a0712b5b0d5a1
xuwenchao <842390367@qq.com>
2022-06-06 11:51:25 +0800

    fix: 🐛 use __dirname for pkg-dir

--- server/src/common/config/index.ts
+++ server/src/common/config/index.ts
@@ -3,9 +3,9 @@ import { sync } from 'pkg-dir';
 
 export const API_PREFIX = '/api';
 
-console.debug('file name', join(sync(), './whistle.modify.json'));
+console.debug('file name', join(sync(__dirname), './whistle.modify.json'));
 
 export const DB = {
   version: 'v1',
-  fileName: join(sync(), './whistle.modify.json'),
+  fileName: join(sync(__dirname), './whistle.modify.json'),
 };
