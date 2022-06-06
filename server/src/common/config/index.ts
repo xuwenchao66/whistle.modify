9767f3a6b62a82ec60ff7542f41d1b2eeae53f56
xuwenchao <842390367@qq.com>
2022-06-06 11:40:49 +0800

    test: 💍 debug log

--- server/src/common/config/index.ts
+++ server/src/common/config/index.ts
@@ -3,6 +3,8 @@ import { sync } from 'pkg-dir';
 
 export const API_PREFIX = '/api';
 
+console.debug('file name', join(sync(), './whistle.modify.json'));
+
 export const DB = {
   version: 'v1',
   fileName: join(sync(), './whistle.modify.json'),
