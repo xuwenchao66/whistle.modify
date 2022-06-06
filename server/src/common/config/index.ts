0ea3973af3a7df1fd9613b4908631ba787c20644
xuwenchao <842390367@qq.com>
2022-06-06 11:55:17 +0800

    fix: 🐛 remove debug log

--- server/src/common/config/index.ts
+++ server/src/common/config/index.ts
@@ -3,8 +3,6 @@ import { sync } from 'pkg-dir';
 
 export const API_PREFIX = '/api';
 
-console.debug('file name', join(sync(__dirname), './whistle.modify.json'));
-
 export const DB = {
   version: 'v1',
   fileName: join(sync(__dirname), './whistle.modify.json'),
