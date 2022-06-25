821a947da98035099f737321c45455cb72e91a32
xuwenchao <842390367@qq.com>
2022-06-25 19:15:24 +0800

    fix: 🐛 add exclude for ServeStaticModule

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -3,12 +3,16 @@ import { ServeStaticModule } from '@nestjs/serve-static';
 import { Module } from '@nestjs/common';
 import { RuleModule } from './models/rule/rule.module';
 import { GroupModule } from './models/group/group.module';
+import { API_PREFIX } from './common/config';
 
 const staticRootPath = join(__dirname, '../client');
 
 @Module({
   imports: [
-    ServeStaticModule.forRoot({ rootPath: staticRootPath }),
+    ServeStaticModule.forRoot({
+      rootPath: staticRootPath,
+      exclude: [`${API_PREFIX}*`],
+    }),
     RuleModule,
     GroupModule,
   ],
